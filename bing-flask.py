74a47791ed449159f3f0d20975106da30a5f6e93
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-16 16:24:46 +0330

    ADD new route to show pics and download them

--- bing-flask.py
+++ bing-flask.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
+import os
 import backgroundify as bg
 
 app = Flask(__name__)
@@ -14,6 +15,22 @@ def index():
         else bg.get_img_url(day=day)
     )
     return response
+
+
+# can save pictures of previous days (up to 8 days bc of Bing restrictions). /?day={int}
+@app.route("/save")
+def save():
+    file_path = os.getcwd() + "/static/pic/"
+    day = request.args.get("day", default=1, type=int)
+    day = day if day <= 8 else 1
+    img_url = bg.get_img_url(day=day)
+    filename = bg.get_filename(days=day - 1)
+    if os.path.exists(file_path + filename):
+        return send_file(file_path + filename, mimetype="image/jpg")
+    else:
+        return send_file(
+            bg.save_file(img_url, file_path, filename), mimetype="image/jpg"
+        )
 
 
 if __name__ == "__main__":
