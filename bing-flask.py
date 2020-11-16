ce90b09e424c99a8528ebfac1a5798f2b4f01230
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-16 09:35:44 +0330

    UPDATE parameter for picture of previous days

--- bing-flask.py
+++ bing-flask.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 import backgroundify as bg
 
 app = Flask(__name__)
 
-
+# can get pictures of previous days (up to 8 days bc of Bing restrictions). /?day={int}
 @app.route("/")
 def index():
-    return bg.get_img_url()
+    day = request.args.get("day", default=1, type=int)
+    day = day if day <= 8 else 1
+    return bg.get_img_url(day=day)
 
 
 if __name__ == "__main__":
