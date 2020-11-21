9c107ad018a81cdfd80fd90dcc649e33b87be163
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-21 22:49:33 +0330

    ADD new route for api calls

--- bing-flask.py
+++ bing-flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, url_for
 from backgroundify import Backgroundify
 import os
 
@@ -25,6 +25,22 @@ def save(days):
     file_path = os.getcwd() + "/static/pic/"
     filename = bg.imgs[0]["filename"]
     return send_file(file_path + filename, mimetype="image/jpg")
+
+
+@app.route("/api/<int:days>")
+def api(days):
+    response = {}
+    bg = Backgroundify(days=days)
+    bg.get_imgs()
+    bg.save_files()
+    for i in range(days):
+        response[i] = {
+            "url": "localhost:5000"
+            + url_for("static", filename="pic/" + bg.imgs[i]["filename"]),
+            "title": bg.imgs[i]["title"],
+            "copyright": bg.imgs[i]["copyright"],
+        }
+    return response
 
 
 if __name__ == "__main__":
