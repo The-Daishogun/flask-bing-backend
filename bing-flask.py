1820944e117f3d2dba4d5a737b796e741e1c12d2
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-21 22:16:01 +0330

    REFACTOR to work with the new ver of Backgroundify

--- bing-flask.py
+++ bing-flask.py
@@ -1,36 +1,30 @@
 from flask import Flask, request, send_file
+from backgroundify import Backgroundify
 import os
-import backgroundify as bg
 
 app = Flask(__name__)
 
-# can get pictures of previous days (up to 8 days bc of Bing restrictions). /?day={int}
-@app.route("/")
-def index():
-    day = request.args.get("day", default=1, type=int)
-    day = day if day <= 8 else 1
+# can get pictures of previous days (up to 8 days bc of Bing restrictions).
+@app.route("/<int:days>")
+def index(days):
+    bg = Backgroundify(days=days)
     response = (
-        {"url": bg.get_img_url(day=day)}
+        bg.get_imgs()
         if request.content_type == "application/json"
-        else bg.get_img_url(day=day)
+        else str(bg.get_imgs())
     )
     return response
 
 
-# can save pictures of previous days (up to 8 days bc of Bing restrictions). /?day={int}
-@app.route("/save")
-def save():
+# can save pictures of previous days (up to 8 days bc of Bing restrictions). saves all the files up to that day but only serves the latest pic
+@app.route("/save/<int:days>")
+def save(days):
+    bg = Backgroundify(days=days)
+    bg.get_imgs()
+    bg.save_files()
     file_path = os.getcwd() + "/static/pic/"
-    day = request.args.get("day", default=1, type=int)
-    day = day if day <= 8 else 1
-    img_url = bg.get_img_url(day=day)
-    filename = bg.get_filename(days=day - 1)
-    if os.path.exists(file_path + filename):
-        return send_file(file_path + filename, mimetype="image/jpg")
-    else:
-        return send_file(
-            bg.save_file(img_url, file_path, filename), mimetype="image/jpg"
-        )
+    filename = bg.imgs[0]["filename"]
+    return send_file(file_path + filename, mimetype="image/jpg")
 
 
 if __name__ == "__main__":
